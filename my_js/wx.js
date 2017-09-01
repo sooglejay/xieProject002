7ce1412350432c7a728e4375a78e440f06f889de
sooglejay <394055524@qq.com>
2017-09-01 16:17:23 +0800

    test get location

--- my_js/wx.js
+++ my_js/wx.js
@@ -78,18 +78,33 @@ function wxConfig(res) {
         wx.showMenuItems({
             menuList: []    //要显示的菜单项
         });
-        wx.getLocation({
+        wx.checkJsApi({
+            jsApiList: [
+                'getLocation'
+            ],
             success: function (res) {
-                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                var speed = res.speed; // 速度，以米/每秒计
-                var accuracy = res.accuracy; // 位置精度
-                alert(latitude + "" + longitude);
-            },
-            cancel: function (res) {
-                alert('用户拒绝授权获取地理位置');
+                // alert(JSON.stringify(res));
+                // alert(JSON.stringify(res.checkResult.getLocation));
+                if (res.checkResult.getLocation == false) {
+                    alert('你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！');
+                    return;
+                }else{
+                    wx.getLocation({
+                        success: function (res) {
+                            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                            var speed = res.speed; // 速度，以米/每秒计
+                            var accuracy = res.accuracy; // 位置精度
+                            alert(latitude + "-哈哈哈-" + longitude);
+                        },
+                        cancel: function (res) {
+                            alert('用户拒绝授权获取地理位置');
+                        }
+                    });
+                }
             }
         });
+
     });
 
 }
