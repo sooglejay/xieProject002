b6152bdcce58d348a3752aba9a11a7920a784c9f
sooglejay <394055524@qq.com>
2017-09-01 16:23:23 +0800

    fix  location

--- my_js/wx.js
+++ my_js/wx.js
@@ -52,7 +52,6 @@ function wxConfig(res) {
             'showMenuItems',
             'hideOptionMenu',
             'showOptionMenu',
-            'checkJsApi',
             'openLocation',
             'getLocation'
         ]
@@ -95,7 +94,8 @@ function wxConfig(res) {
                             var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                             var speed = res.speed; // 速度，以米/每秒计
                             var accuracy = res.accuracy; // 位置精度
-                            alert(latitude + "-哈哈哈-" + longitude);
+                            LocationApp.lng = longitude;
+                            LocationApp.lat = latitude;
                         },
                         cancel: function (res) {
                             alert('用户拒绝授权获取地理位置');
