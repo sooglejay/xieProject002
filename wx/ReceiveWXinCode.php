bb4ef0fbc3029e5de0b49887fe4b686242e6c4c1
sooglejay <394055524@qq.com>
2017-09-14 23:57:52 +0800

    fix all

--- wx/ReceiveWXinCode.php
+++ wx/ReceiveWXinCode.php
@@ -8,28 +8,47 @@
  */
 class ReceiveWXinCode
 {
-    private function w($text)
+    private function w($resArr)
     {
-        $myfile = fopen("./test.txt", "w") or die("Unable to open file!");
-        fwrite($myfile, $text);
-        fclose($myfile);
+        file_put_contents('./test.txt', print_r($resArr, true));
+    }
+
+    private function set($name, &$value, &$res)
+    {
+        if (isset($_REQUEST[$name])) {
+            $value = $_REQUEST[$name];
+            $res[$name] = $value;
+        }
     }
 
     /**
-     * ReceiveWXinCode constructor.
+     * ReceiveWXinAccessToken constructor.
      */
     public function __construct()
     {
-
+        $access_token = -1;
+        $token_type = -1;
+        $expires_in = -1;
+        $refresh_token = -1;
+        $refresh_token_expires_in = -1;
+        $scope = -1;
+        $business_id = -1;
+        $public_account_id = -1;
         $code = -1;
         $state = -1;
-        if (isset($_REQUEST['code'])) {
-            $code = $_REQUEST['code'];
-        }
-        if (isset($_REQUEST['state'])) {
-            $state = $_REQUEST['state'];
-        }
-        $this->w($code . "-" . $state);
+
+        $res = array();
+        $this->set('access_token', $access_token, $res);
+        $this->set('token_type', $token_type, $res);
+        $this->set('expires_in', $expires_in, $res);
+        $this->set('refresh_token', $refresh_token, $res);
+        $this->set('refresh_token_expires_in', $refresh_token_expires_in, $res);
+        $this->set('scope', $scope, $res);
+        $this->set('business_id', $business_id, $res);
+        $this->set('public_account_id', $public_account_id, $res);
+        $this->set('code', $code, $res);
+        $this->set('state', $state, $res);
+        $this->w($res);
     }
 }
 
