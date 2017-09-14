af8138dbcad657d0092adf33358b8d6e43db4fd4
sooglejay <394055524@qq.com>
2017-09-14 11:18:10 +0800

    fix csv

---
+++ import/ImportCSV.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sooglejay
+ * Date: 17/9/14
+ * Time: 10:47
+ */
+
+date_default_timezone_set("PRC");
+
+require_once './../lib/PHPExcel_1_7_9/Classes/PHPExcel/IOFactory.php';
+require_once './../lib/PHPExcel_1_7_9/Classes/PHPExcel.php';
+
+require_once "./../bootstrap.php";
+require_once "./../model/User.php";
+require_once "./../model/BuyTypeUser.php";
+require_once "./../model/ActivitySepUser.php";
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', 300000); //300 seconds = 5 minutes
+class Imp extends App
+{
+
+    private $userRepo;
+
+    /**
+     * ImportCSV constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->userRepo = $this->entityManager->getRepository('User');
+        $this->admin_import();
+    }
+
+    private function admin_import()
+    {
+
+        $file = "./9_12.csv";
+        $handle = fopen($file, "r");
+        $firstItem = true;
+        $k = 0;
+        $len = 0;
+        if ($file != NULL) {
+            $activitySepRepo = $this->entityManager->getRepository("Shop");
+            if ($activitySepRepo instanceof ShopRepository) {
+                $existsArr = $activitySepRepo->findAll();
+                foreach ($existsArr as $entity) {
+                    if ($entity instanceof Shop) {
+                        $userE = $entity->getShopUser();
+                        if ($userE instanceof User) {
+                            $userE->clearShops();
+                        }
+                        $this->entityManager->remove($entity);
+                    }
+                }
+                $this->entityManager->flush();
+            }
+            while (($row = fgetcsv($handle, 1000, ",")) !== false) {
+                if ($firstItem) {
+                    $firstItem = false;
+                    continue;
+                }
+                $userName = $row[6];
+                $userEntity = $this->userRepo->findOneBy(array("account_name" => $userName));
+                $k++;
+                $shop = new Shop();
+                $shop->setShopUniqueCode(isset($row[7]) ? $row[7] : "");
+                $shop->setShopName(isset($row[8]) ? $row[8] : "");
+                $shop->setShopAddr(isset($row[9]) ? $row[9] : "");
+                $shop->setShopContact1(isset($row[10]) ? $row[10] : "");
+                $shop->setShopContact2(isset($row[11]) ? $row[11] : "");
+                $shop->setShopType(isset($row[12]) ? $row[12] : "");
+                $shop->setShopStreet(isset($row[13]) ? $row[13] : "");
+                $shop->setShopLng(isset($row[14]) ? $row[14] : "");
+                $shop->setShopLat(isset($row[15]) ? $row[15] : "");
+                $shop->setShop280(isset($row[16]) ? $row[16] : "");
+                $shop->setShop209(isset($row[17]) ? $row[17] : "");
+                $shop->setShopGroupNet(isset($row[18]) ? $row[18] : "");
+                $shop->setShopMemNum(isset($row[19]) ? $row[19] : "");
+                $shop->setShopBroadbandCover(isset($row[20]) ? $row[20] : "");
+                $shop->setShopLandline(isset($row[21]) ? $row[21] : "");
+                $shop->setShopOperator(isset($row[22]) ? $row[22] : "");
+                $shop->setTime(strtotime(isset($row[23]) ? $row[23] : date("m/d/Y H:i:s") . ""));
+                $shop->setShopUser($userEntity);
+                $this->entityManager->persist($shop);
+                if ($k % 1000 == 0) {
+                    $this->entityManager->flush();
+                    $k = 0;
+                }
+                $len++;
+            }
+            if ($k > 0) {
+                $this->entityManager->flush();
+            }
+            echo "\n size = " . $len . "\n";
+        }
+    }
+}
+
+new Imp();
+?>
