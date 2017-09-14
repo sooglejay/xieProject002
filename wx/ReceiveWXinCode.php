4d6c95d567a4c902a3efeef081f7a374e59d5eaa
sooglejay <394055524@qq.com>
2017-09-14 20:26:09 +0800

    测试在微盟上新建一个应用，然后授权给微信公众号，并设置回调接口，接收微盟传过来的code和state，详情见:http://open.weimob.com/doc/article?tag=ab

--- wx/ReceiveWXinCode.php
+++ wx/ReceiveWXinCode.php
@@ -8,8 +8,9 @@
  */
 class ReceiveWXinCode
 {
-    private function w($text){
-        $myfile = fopen("/ziyan/wx/test.txt", "w") or die("Unable to open file!");
+    private function w($text)
+    {
+        $myfile = fopen("./test.txt", "w") or die("Unable to open file!");
         fwrite($myfile, $text);
         fclose($myfile);
     }
@@ -19,9 +20,12 @@ class ReceiveWXinCode
      */
     public function __construct()
     {
-        $code = $_REQUEST['code'];
-        $state = $_REQUEST['state'];
-        $this->w($code."-",$state);
+
+        $code = -1;
+        $state = -1;
+        if (isset($_REQUEST['code'])) $code = $_REQUEST['code'];
+        if (isset($_REQUEST['state'])) $state = $_REQUEST['state'];
+        $this->w($code . "-", $state);
     }
 }
 
