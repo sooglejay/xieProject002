cc6750560a2e8a1185c71a95434d3cc408ab2533
sooglejay <394055524@qq.com>
2017-09-19 15:17:42 +0800

    fix  测试能否收到微信消息

--- wx/validate/demo.php
+++ wx/validate/demo.php
@@ -5,6 +5,9 @@
 class demo
 {
 
+    /**
+     * 次方法只是用于微信的接入验证，验证过后，就不用了。
+     */
     public function checkSignature()
     {
         $signature = $_GET['signature']; //微信加密签名
@@ -21,50 +24,23 @@ class demo
         }
     }
 
-//    function doCheck($signature, $timeStamp, $nonce, $echostr)
-//    {
-//        // 第三方发送消息给公众平台
-//        $encodingAesKey = "SRNPxS03InrOhHS0WSSLqe8VkQLccJvnLfsZoizgesD";
-//        $token = "asvw5tsdfgvw456e";
-//        $appId = "wxb76c5258ffa59386";
-////        $text = "<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>";
-////
-//        $pc = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
-////        $encryptMsg = '';
-//        $errCode = $pc->encryptMsg($text, $timeStamp, $nonce, $encryptMsg);
-////        if ($errCode == 0) {
-////            print("加密后: " . $encryptMsg . "\n");
-////        } else {
-////            print($errCode . "\n");
-////        }
-//
-//        $xml_tree = new DOMDocument();
-//        $xml_tree->loadXML($encryptMsg);
-//        $array_e = $xml_tree->getElementsByTagName('Encrypt');
-//        $array_s = $xml_tree->getElementsByTagName('MsgSignature');
-//        $encrypt = $array_e->item(0)->nodeValue;
-//        $msg_sign = $array_s->item(0)->nodeValue;
-//
-//        $format = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><Encrypt><![CDATA[%s]]></Encrypt></xml>";
-//        $from_xml = sprintf($format, $encrypt);
-//
-//        // 第三方收到公众号平台发送的消息
-//        $msg = '';
-//        $errCode = $pc->decryptMsg($msg_sign, $timeStamp, $nonce, $from_xml, $msg);
-//        if ($errCode == 0) {
-//            print("解密后: " . $msg . "\n");
-//        } else {
-//            print($errCode . "\n");
-//        }
-//
-//    }
+
+    function doCheck()
+    {
+
+        $data = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!is_null($data)) {//接收消息并处理
+            $xml = (array)simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
+            file_put_contents('./test.txt', print_r($xml, true));
+        }
+    }
 
     /**
      * demo constructor.
      */
     public function __construct()
     {
-        $this->checkSignature();
+        $this->doCheck();
     }
 }
 
