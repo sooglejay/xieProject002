a755a28df45f6f7f43046dfc89b4370b68737b28
sooglejay <394055524@qq.com>
2017-09-20 17:21:51 +0800

    fix all

--- my_js/login.js
+++ my_js/login.js
@@ -22,7 +22,6 @@ function checkLogin() {
             box.loadding('加载中...');
         },
         success: function (res) {
-            $(".login-body").show();
             layer.closeAll();
             openId = res["openId"];
             if (openId.hasOwnProperty("0")) {
@@ -31,11 +30,12 @@ function checkLogin() {
             if (res.code == 200) {
                 window.location.href = '/ziyan/home.html?openId=' + openId;
             }
+            $(".login-body").show();
         },
         error: function (e) {
-            $(".login-body").show();
             layer.closeAll();
             console.log(e);
+            $(".login-body").show();
         }
     });
 }
