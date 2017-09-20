8264da73ff90d250dd8ffaf80baa7dde1ab9c456
sooglejay <394055524@qq.com>
2017-09-20 16:29:55 +0800

    重新测试

--- my_js/wx.js
+++ my_js/wx.js
@@ -74,36 +74,38 @@ function wxConfig(res) {
      */
     wx.ready(function () {
         // wx.hideAllNonBaseMenuItem();    //隐藏所有非基础按钮接口
-        // wx.showMenuItems({
-        //     menuList: []    //要显示的菜单项
-        // });
-        // wx.checkJsApi({
-        //     jsApiList: [
-        //         'getLocation'
-        //     ],
-        //     success: function (res) {
-        //         // alert(JSON.stringify(res));
-        //         // alert(JSON.stringify(res.checkResult.getLocation));
-        //         if (res.checkResult.getLocation == false) {
-        //             alert('你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！');
-        //             return;
-        //         }else{
-        //             wx.getLocation({
-        //                 success: function (res) {
-        //                     var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-        //                     var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-        //                     var speed = res.speed; // 速度，以米/每秒计
-        //                     var accuracy = res.accuracy; // 位置精度
-        //                     LocationApp.lng = longitude;
-        //                     LocationApp.lat = latitude;
-        //                 },
-        //                 cancel: function (res) {
-        //                     alert('用户拒绝授权获取地理位置');
-        //                 }
-        //             });
-        //         }
-        //     }
-        // });
+        wx.showMenuItems({
+            // menuList: []    //要显示的菜单项
+            menuList: ["menuItem:share:appMessage", "menuItem:share:timeline"]    //要显示的菜单项
+
+        });
+        wx.checkJsApi({
+            jsApiList: [
+                'getLocation'
+            ],
+            success: function (res) {
+                // alert(JSON.stringify(res));
+                // alert(JSON.stringify(res.checkResult.getLocation));
+                if (res.checkResult.getLocation == false) {
+                    alert('你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！');
+                    return;
+                }else{
+                    wx.getLocation({
+                        success: function (res) {
+                            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                            var speed = res.speed; // 速度，以米/每秒计
+                            var accuracy = res.accuracy; // 位置精度
+                            LocationApp.lng = longitude;
+                            LocationApp.lat = latitude;
+                        },
+                        cancel: function (res) {
+                            alert('用户拒绝授权获取地理位置');
+                        }
+                    });
+                }
+            }
+        });
 
     });
 
