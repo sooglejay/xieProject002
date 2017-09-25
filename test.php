89aad8ee393dc77e6a0008f20207ea4a5a9df0c1
sooglejay <394055524@qq.com>
2017-09-25 12:35:25 +0800

    修改文件引用路径

--- test.php
+++ test.php
@@ -6,8 +6,10 @@
 // * Time: 21:56
 // */
 ////date_default_timezone_set("PRC");
-////echo strtotime("6/30/2017 17:53:53");
-////echo  date("m/d/Y H:i:s");
+echo strtotime("6/30/2017 17:53:53");
+
+
+echo  date("m/d/Y H:i:s",strtotime("6/30/2017 17:53:53"));
 ////echo dirname(__FILE__)
 ////;
 //
@@ -63,28 +65,4 @@
 //
 //
 
-$key="liangshanyidong!@#$%12345^&*9678";
-//$key previously generated safely, ie: openssl_random_pseudo_bytes
-$plaintext = '{"Lid":1,"Password":"e10adc3949ba59abbe56e057f20f883e","mobile":"15928620723","userId":"user"}';
-$ivlen = openssl_cipher_iv_length($cipher="AES-256-CBC");
-$iv = openssl_random_pseudo_bytes($ivlen);
-$ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-
-echo $ciphertext_raw."\n\n";
-$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-$ciphertext = base64_encode( $iv.$hmac.$ciphertext_raw );
-$ciphertext='OENDODU1MDg2QzNFM0I2MzdERjY0NUQ5NTJGMkUwRTY2NUE2M0UxNTUzOEM1NjU0QzM5MkU2OTlBRTI0QUQzODlGQTVBNzdCQThEQTc0NUI4NjBCQzhCMTQ1QTJDNTEwQ0FDNjQwRTIxQjNDOEMwRTVGODU3NTRFOEQzOUUzM0M4RTU3NDY3QjlGREM3NkY0NjI0ODhFQkFBOUFGRDBENTYwMjJFMEMzMkFDRTE4MzlFRTRFODEwQkFBRTUzQzY2';
-echo $ciphertext."\n\n";
-//decrypt later....
-$c = base64_decode($ciphertext);
-$ivlen = openssl_cipher_iv_length($cipher="AES-256-CBC");
-$iv = substr($c, 0, $ivlen);
-$hmac = substr($c, $ivlen, $sha2len=32);
-$ciphertext_raw = substr($c, $ivlen+$sha2len);
-$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-if (hash_equals($hmac, $calcmac))//PHP 5.6+ timing attack safe comparison
-{
-    echo $original_plaintext."\n";
-}
 ?>
