166c4ed91deb2be77130df565986d23cff1546f7
sooglejay <394055524@qq.com>
2017-10-26 20:58:19 +0800

    test access_token

--- test.php
+++ test.php
@@ -6,64 +6,18 @@
 // * Time: 21:56
 // */
 ////date_default_timezone_set("PRC");
-echo strtotime("6/30/2017 17:53:53");
 
-echo phpinfo();
+// create a new cURL resource
+$ch = curl_init();
 
-//echo  date("m/d/Y H:i:s",strtotime("6/30/2017 17:53:53"));
-////echo dirname(__FILE__)
-////;
-//
-//
-//
-////php aes加密类
-//class AESMcrypt {
-//    public $iv = null;
-//    public $key = null;
-//    public $bit = 128;
-//    private $cipher;
-//    public function __construct($bit, $key, $iv, $mode) {
-//        if(empty($bit) || empty($key) || empty($iv) || empty($mode))
-//            return NULL;
-//        $this->bit = $bit;
-//        $this->key = $key;
-//        $this->iv = $iv;
-//        $this->mode = $mode;
-//        switch($this->bit) {
-//            case 192:$this->cipher = MCRYPT_RIJNDAEL_192; break;
-//            case 256:$this->cipher = MCRYPT_RIJNDAEL_256; break;
-//            default: $this->cipher = MCRYPT_RIJNDAEL_128;
-//        }
-//        switch($this->mode) {
-//            case 'ecb':$this->mode = MCRYPT_MODE_ECB; break;
-//            case 'cfb':$this->mode = MCRYPT_MODE_CFB; break;
-//            case 'ofb':$this->mode = MCRYPT_MODE_OFB; break;
-//            case 'nofb':$this->mode = MCRYPT_MODE_NOFB; break;
-//            default: $this->mode = MCRYPT_MODE_CBC;
-//        }
-//    }
-//    public function encrypt($data) {
-//        $data = base64_encode(mcrypt_encrypt( $this->cipher, $this->key, $data, $this->mode, $this->iv));
-//        return $data;
-//    }
-//    public function decrypt($data) {
-//        $data = mcrypt_decrypt( $this->cipher, $this->key, base64_decode($data), $this->mode, $this->iv);
-//        $data = rtrim(rtrim($data));
-//        return $data;
-//    }
-//}
-////使用方法
-//$aes = new AESMcrypt($bit = 128, $key = 'liangshanyidong!@#$%12345^&*9678', $iv = '0987654321fedcba', $mode = 'cbc');
-//$pas = '{"Lid":1,"Password":"e10adc3949ba59abbe56e057f20f883e","mobile":"15928620723","userId":"user"}';
-//$c = $aes->encrypt($pas);
-//var_dump($c);
-//echo "\n\n";
-//var_dump($aes->decrypt($c));
-//
-//
-//
-//
-//
-//
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxb76c5258ffa59386&secret=80b8d4e879263070071d9736d511f3b5");
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
+// grab URL and pass it to the browser
+$s = curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
+echo $s;
 ?>
