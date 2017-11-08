5473bd76fc487ca714ca9ecbc5721642f08c2445
sooglejay <394055524@qq.com>
2017-11-08 11:05:43 +0800

    fix

--- wx/validate/demo.php
+++ wx/validate/demo.php
@@ -38,10 +38,9 @@ class demo
     {
         $this->fileCachePath = dirname(__FILE__) . '/../file_cache/openId.txt';
         $wholeFile = dirname(__FILE__) . '/../file_cache/wholeText.txt';
-        file_put_contents($wholeFile, json_encode(array("jiangwei"=>"say hello")));
-        echo "1212";
         $data = file_get_contents("php://input");
         $xml = simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
+        file_put_contents($wholeFile, json_encode($xml));
         if (isset($xml) &&
             isset($xml->FromUserName) &&
             isset($xml->ToUserName) &&
