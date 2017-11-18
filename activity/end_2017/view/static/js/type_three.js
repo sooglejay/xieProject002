4da244e4b23726a13748c96032a908ca506fbe8e
sooglejay <394055524@qq.com>
2017-11-18 11:58:01 +0800

    fix  logic

--- activity/end_2017/view/static/js/type_three.js
+++ activity/end_2017/view/static/js/type_three.js
@@ -1,3 +1,55 @@
 /**
  * Created by sooglejay on 17/11/16.
  */
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null
+}
+function submit(activityCode) {
+    var phoneNumber = getURLParameter('phoneNumber');
+    $.ajax({
+        url: './../../controller/OrderApp.php',
+        type: 'GET',
+        data: {
+            type: 3,
+            phoneNumber: phoneNumber,
+            activityCode: activityCode
+        },
+        dataType: 'json',
+        beforeSend: function () {
+            box.loadding("正在办理,请稍后...");
+        },
+        success: function (res) {
+            layer.closeAll();
+            if (res.code == 200) {
+                $('#myModal').modal('show');
+                $(".modal-body").html('办理成功！');
+            } else {
+                $('#myModal').modal('show');
+                $(".modal-body").html(res.message);
+            }
+            console.log(res);
+        },
+        error: function (res) {
+            layer.closeAll();
+            console.log(res);
+        }
+    });
+}
+
+$(function () {
+    $(".dismissImg").click(function () {
+        $('#myModal').modal('hide');
+    });
+
+    $('#btnSubmitThree_a').click(function () {
+        submit('LLZS20');
+    });
+
+    $('#btnSubmitThree_b').click(function () {
+        submit('LLZS30');
+    });
+    $('#btnSubmitThree_c').click(function () {
+        submit('LLJB50');
+    });
+
+});
