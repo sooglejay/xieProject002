00269bb42f0ef362085b344efd80d87bd9182d10
sooglejay <394055524@qq.com>
2017-11-20 15:13:15 +0800

    add ba zhong

---
+++ wx/validate/bazhong/validation.php
@@ -0,0 +1,42 @@
+<?php
+
+class ValidationApp
+{
+    /**
+     * 次方法只是用于微信的接入验证，验证过后，就不用了。
+     */
+    public function checkSignature()
+    {
+        $signature = $_GET['signature']; //微信加密签名
+        $timestamp = $_GET['timestamp']; //时间戳
+        $nonce = $_GET['nonce']; //随机数
+        $echostr = $_GET['echostr']; //随机字符串
+        $token = "ZiYanWeiXinGongZhongHaoToken";
+        $tempArr = array($token, $timestamp, $nonce);
+        sort($tempArr);
+        if ($signature == sha1(implode($tempArr))) {
+            echo $echostr;
+        } else {
+            exit();
+        }
+    }
+
+    /***
+     * demo constructor.
+     */
+    public function __construct()
+    {
+        if (isset($_GET["signature"])) {
+            $this->checkSignature();
+            $data = $_GET["signature"];
+            file_put_contents(dirname(__FILE__) . '/signature.txt', json_encode($data));
+
+            return;
+        }
+        $data = file_get_contents("php://input");
+        file_put_contents(dirname(__FILE__) . '/bazhong_input.txt', json_encode($data));
+
+    }
+}
+
+$r = new ValidationApp();
